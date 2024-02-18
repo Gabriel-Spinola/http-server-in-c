12fd131203ee2174a83ddd6478334112f217d4bc
Vyctor Huggo <vyctorhuggo322@gmail.com>
2024-02-18 16:50:12 -0300

    noite

--- src/database/database.c
+++ src/database/database.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <sqlite3.h>
 
+int main()
+{
+    char* err;
+    sqlite3* db;
+    sqlite3_stmt* stmt;
+    sqlite3_open("database.db", &db);
+
+    int rc = sqlite3_exec(
+        db, 
+        "CREATE TABLE IF NOT EXISTS algolancer(x1 INT, x2 INT, name varchar(60));", 
+        NULL, 
+        NULL, 
+        &err
+    );
+
+    if(rc != SQLITE_OK) {
+        printf("%c", *err);
+    }
+}
---
+++ src/database/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE example_table (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age int
+);
+
+INSERT INTO example_table (name, age) VALUES ('Test UserName', 23);
+INSERT INTO example_table (name, age) VALUES ('Mark Silva', 24);
+INSERT INTO example_table (name, age) VALUES ('Hector Kamara', 26);
+INSERT INTO example_table (name, age) VALUES ('Greg Anthony', 45);
+INSERT INTO example_table (name, age) VALUES ('Vyctor Huggo', 16);
