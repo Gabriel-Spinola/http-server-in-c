365627b8c57af4e10ad91fa6b89e9dabdc874ae2
wnccys <ipooo346@gmail.com>
2024-03-01 20:49:38 -0300

    Feat: correctly set check_db base function

--- src/db_test/db.c
+++ src/db_test/db.c
@@ -1,35 +1,55 @@
 #include "db.h"
-#include<stdio.h>
-#include<string.h>
-#include<stdbool.h>
-
-void check_db(); // => JSON_OBJ, 
+#include <stdlib.h>
 
 int main(){
-    check_db();
+    check_db(2);
 
     return 0;
 }
 
 // Returns user transaction/extract JSON from DB (prototype for further DB);
-void check_db (){
+// Basically a demonstration to use jsonify_data and tokenize_data;
+int check_db(int id){
+    //... { Connect with DB }
+    //... { Make query to get rows }
     FILE* fp;
     fp = fopen("user_db.csv", "r");
+    if (fp == NULL){
+        printf("Couldn't read file");
+        return 1;
+    }
 
     char row[MAXCHAR];
-    char* token;
+    char headers[MAXCHAR];
+    // Ignores headers row;
+    int targered_row = id+1;
+    int current_row = 1;
 
-    while(feof(fp) != true) 
-    {
+    // Get row of id;
+    while (current_row <= targered_row){
         fgets(row, MAXCHAR, fp);
-        printf("Row: %s\n", row);
-
-        token = strtok(row, ",");
-
-        while(token != NULL)
-        {
-            printf("Token %s\n", token);
-            token = strtok(NULL, ",");
-        }
+        // Gets header row;
+        if (current_row == 1){ memcpy(headers, row, strlen(row)); };
+        current_row++;
     }
+    if (row == NULL){
+        printf("Couldn't read row");
+        return 1;
+    }
+
+    free(fp);
+
+    remove_spaces(row); remove_spaces(headers);
+
+    printf("Targered Row: %s\n", row);
+    printf("Row Headers: %s", headers);
+
+    jsonify_data(headers, row);
+}
+
+// Receives DB table headers and query data to make JSON struct:
+// headers[1] + bodies[1] => { headers[1]: bodies[1] }
+// Can be used recursively;
+void jsonify_data(char headers[], char bodies[]){
+
 }
--- src/db_test/db.h
+++ src/db_test/db.h
@@ -1,6 +1,13 @@
 #ifndef DB_H 
-#define DB_H 
+#define DB_H
 
-    #define MAXCHAR 500
+    #define MAXCHAR 50
+    #include <stdio.h>
+    #include <string.h>
+    #include <stdbool.h>
+    #include "utils.c"
+    int check_db(int id);
+    void tokenize_args(char headers[], char bodies[]);
+    void jsonify_data(char headers[], char bodies[]);
 
 #endif
