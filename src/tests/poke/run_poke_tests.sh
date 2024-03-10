42b560bcc29fe518c6b5f0578e0e3e29710f3d49
wnccys <ipooo346@gmail.com>
2024-03-09 21:40:03 -0300

    Fix: fixes to local parameters

--- src/tests/poke/run_poke_tests.sh
+++ src/tests/poke/run_poke_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+poke=$HOME/opt/poke
 for test_file in *.json; do
     if [[ -f "$test_file" ]]; then
         echo "SENDING REQUEST: $test_file"
-        poke net send -f "$test_file"
+        "$poke" net send -f "$test_file"
     fi
 done
